e86ca908799c3a099825efcddbc5318adcb9aac3
kuli22te <kuli22te@student.ju.se>
2023-04-11 17:08:14 +0200

    2023-04-11

--- functions.js
+++ functions.js
@@ -2,32 +2,6 @@ const toDoItems = document.getElementsByClassName("to-do-items")[0];
 const input = document.getElementById("input");
 const btn = document.getElementById("add-btn");
 
-//Load items from local storage on page load
-window.addEventListener("load", function () {
-  const items = this.localStorage.getItem("items");
-  if (items) {
-    toDoItems.innerHTML = items;
-
-    //Restore check and trash icons statmen
-    const itemList = toDoItems.querySelector(".item");
-    itemsList.array.forEach(function (item) {
-      const checkIcon = item.querySelector(".fa-solid.fa-check");
-      if (item.dataset.checked === "true") {
-        checkIcon.style.color = "limegreen";
-      }
-      const trashIcon = item.querySelector(".fa-solid.fa-trash");
-      trashIcon.addEventListener("click", function () {
-        item.remove();
-        saveItems();
-      });
-      checkIcon.addEventListener("click", function () {
-        toggleCheckicon(checkIcon, item);
-        saveItems();
-      });
-    });
-  }
-});
-
 input.addEventListener("keydown", function (event) {
   if (event.key === "Enter") addItem();
 });
@@ -43,8 +17,26 @@ function addItem() {
   var trashIcon = document.createElement("i");
 
   divParent.className = "item";
-  divParent.dataset.checked = "false"; //
   divParent.innerHTML = "<div>" + input.value + "</div>";
 
   checkIcon.className = "fa-solid fa-check";
+  checkIcon.style.color = "lightgrey";
+  CheckIcon.addEventListener("click,", function () {
+    checkIcon.style.color = "limegreen";
+  });
+
+  divChild.appendChild(checkIcon);
+
+  trashIcon.className = "fa-solid fa-trash";
+  trashIcon.style.color = "darkgrey";
+  trashIcon.addEventListener("click", function () {
+    divParent.remove();
+  });
+
+  divChild.appendChild(trashIcon);
+  divParent.appendChild(divChild);
+
+  toDoItems.appendChild(divParent);
+
+  input.value = "";
 }
