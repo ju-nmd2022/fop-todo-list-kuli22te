0df2059037723b47cdda9c4333b860242d54db43
kuli22te <kuli22te@student.ju.se>
2023-04-11 17:27:55 +0200

    2023-04-11

--- functions.js
+++ functions.js
@@ -1,13 +1,8 @@
 const toDoItems = document.getElementsByClassName("to-do-items")[0];
 const input = document.getElementById("input");
-const btn = document.getElementById("add-btn");
 
 input.addEventListener("keydown", function (event) {
   if (event.key === "Enter") addItem();
-});
-
-btn.addEventListener("click", function () {
-  addItem();
 });
 
 function addItem() {
@@ -19,13 +14,13 @@ function addItem() {
   divParent.className = "item";
   divParent.innerHTML = "<div>" + input.value + "</div>";
 
-  checkIcon.className = "fa-solid fa-check";
-  checkIcon.style.color = "lightgrey";
+  CheckIcon.className = "fa-solid fa-check";
+  CheckIcon.style.color = "lightgrey";
   CheckIcon.addEventListener("click,", function () {
-    checkIcon.style.color = "limegreen";
+    CheckIcon.style.color = "limegreen";
   });
 
-  divChild.appendChild(checkIcon);
+  divChild.appendChild(CheckIcon);
 
   trashIcon.className = "fa-solid fa-trash";
   trashIcon.style.color = "darkgrey";
